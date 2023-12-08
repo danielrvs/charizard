cfff5b9078b1ec473909c32635321b28a0d715b9
danielrvs <daniel.rivas.roa@gmail.com>
2023-12-08 11:06:34 +0100

    test: add test for get pokemon from api service

---
+++ app/wp-content/themes/understrap-child/src/Services/Pokemon/GetPokemonFromApiService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Services\Pokemon;
+
+class GetPokemonFromApiService
+{
+
+    /**
+     *  @return array{data: array, message: string}
+     */
+
+    public function handle(): array
+    {
+        return [
+            
+        ];
+    }
+}
